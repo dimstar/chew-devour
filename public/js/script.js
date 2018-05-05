a8fa558db05e8e5205a3c59e5e2849541d7b39c7
dimstar <kylermagee@gmail.com>
2018-05-04 23:53:56 -0700

    added icons and heroku

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,30 @@
 // Frontend Scripting for Burger Joint
+
+let BJ = {
+    init: function(){
+        $('#submitBurger').on('click', function(event){
+            event.preventDefault();
+            BJ.addBurger();
+        });
+    },
+    addBurger: function(){
+        let burger_name = $('#burger-name').val() || false;
+        if(burger_name){
+            $.ajax({
+                url: "/burger",
+                data: {"name": burger_name},
+                method: 'POST'
+              }).done(function() {
+                $( this ).addClass( "done" );
+              });   
+        }
+    },
+    updateBurger: function(){
+
+    },
+    printMssg: function(mssg){
+        $('.mssgOutput').text(mssg).addClass('active');
+    }
+}
+
+BJ.init();
