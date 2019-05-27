195312ef3c7c5e8af2c29a4b6ad254d031c833ac
dimstar <kylermagee@gmail.com>
2019-05-27 10:15:37 -0700

    slowly hacking in sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,43 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'root',
-        password : '',
-        database : 'burger_joint'
-      });
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+require('dotenv').config();
+// const mysql = require('mysql');
+// const connection = mysql.createConnection({
+//         host     : 'localhost',
+//         user     : 'root',
+//         password : '',
+//         database : 'burger_joint'
+//       });
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.RDS_DB_NAME, process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
+  host: process.env.RDS_HOSTNAME,
+  dialect: process.env.RDS_DIALECT,
+  pool: {
+    max: 5,
+    min: 0,
+    aquire: 3000,
+    idle: 1000
+  },
 });
 
+// test connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+
+// Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
 // Export connection for our ORM to use.
-module.exports = connection;
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-// ORM example /Users/kylemagee/Documents/webapps/vvv/www/codebcamp/htdocs/class-repo/01-Class-Content/14-handlebars/01-Activities/12-OrmExample
+  // ORM example /Users/kylemagee/Documents/webapps/vvv/www/codebcamp/htdocs/class-repo/01-Class-Content/14-handlebars/01-Activities/12-OrmExample
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./config/routes.js');
+// @todo implement production/dev environment
+
+let env = process.env.NODE_ENV || "development";
+ 
+
+// @todo think about json referencing like this
+/**
+ * let config = require(__dirname + "../config/congig.json")[env];
+ */
 const PORT = process.env.PORT || 1985;
 
 const app = express();
@@ -15,6 +24,13 @@ app.use(bodyParser.json());
 // middle ware the pub folder
 app.use(express.static('public'));
 
+// @todo This is backwards! do this instead
+/**
+ * Pass the app directly into the router!
+ * const router = require('./config/routes.js')(app);
+ * ----------
+ * module.exports = function(app){ return 'the goods';}
+ */
 router.init(app);
 
 //listen
